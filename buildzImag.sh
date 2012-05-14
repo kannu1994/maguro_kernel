e8d083d8f327146a3d44d8ec60451a2e9188c789
ar <krarvind@gmail.com>
2012-05-14 15:37:59 -0300

    Vibrator control from Ezekeel, turned off and prob won't use, may be for later use

---
+++ buildzImag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp ./arch/arm/configs/tuna_defconfig ./tuna_defconfig
+#make mrproper
+make tuna_defconfig
+make -j4
+cp ./arch/arm/boot/zImage ~/android/aosp/out/target/product/maguro/kernel
